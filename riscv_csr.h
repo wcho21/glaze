0f3c4bb4dd167aec3a7512cdde4556bada268c26
W. Cho <wcho.dev@gmail.com>
2024-09-19 08:45:39 +0900

    support writing mscratch

--- riscv_csr.h
+++ riscv_csr.h
@@ -35,4 +35,9 @@ static inline void write_mtvec(uint64_t x)
   asm volatile("csrw mtvec, %0" : : "r" (x));
 }
 
+static inline void write_mscratch(uint64_t x)
+{
+  asm volatile("csrw mscratch, %0" : : "r" (x));
+}
+
 #endif
